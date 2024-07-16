b5dfd213fc6192d48ec96c6bb8893594f32c0fcf
OM3GA <om3ga6400gd@gmail.com>
2024-07-16 14:15:59 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var win = window.open();
+win.document.body.style.margin = '0';
+win.document.body.style.height = '100vh';
+var iframe = win.document.createElement('iframe');
+iframe.style.border = 'none';
+iframe.style.width = '100%';
+iframe.style.height = '100%';
+iframe.style.margin = '0';
+iframe.src = 'https://github.com/OM3GA6400';
+win.document.body.appendChild(iframe);
+  window.close();
